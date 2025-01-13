8c049f8fbffcf14f7d68043452708a5374fb2d57
Judson Paiva <judson.paiva@hotmail.com>
2025-01-13 06:31:10 +0100

    ganhando produtividade com npm e Express e EJS

--- 1_Primeiros_Passos/app.js
+++ 1_Primeiros_Passos/app.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(request, response){
     response.end("<html><body>Portal de notícias</body></html>");
 });
 
 app.get('/tecnologia', function(request, response){
-    response.end("<html><body>Notícias de Tecnologia</body></html>");
+    // response.end("<html><body>Notícias de Tecnologia</body></html>");
+    response.render("secao/tecnologia");
 });
 
 app.get('/moda', function(request, response){
