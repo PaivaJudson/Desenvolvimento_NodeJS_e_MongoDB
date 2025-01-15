830c59da6cbc95331047eb13b9327a0a1fbe15ba
Judson Paiva <judson.paiva@hotmail.com>
2025-01-15 18:34:29 +0100

    criando conexao e retornando resultados

--- 1_Primeiros_Passos/app/routes/noticias.js
+++ 1_Primeiros_Passos/app/routes/noticias.js
@@ -1,6 +1,22 @@
 
 module.exports = function(app){
+    
     app.get('/noticias', function(request, response){
-        response.render("noticias/noticia.ejs");
+
+        const mysql = require('mysql');
+        const connection = mysql.createConnection({
+            host : '127.0.0.1',
+            user : 'root',
+            password : '123456789',
+            database : 'portal_noticias'
+        });
+    
+        connection.query("select * from noticias", function(error, result){
+            console.log(result);
+            console.log(error);
+
+            response.send(result);
+        });
+        //response.render("noticias/noticia.ejs");
     });
 }
